97005a65c9cd03d89af696d269af912b7d65e800
Ivan Rudnichenki <rudnicenko.ivan@gmail.com>
2022-05-04 20:04:06 +0300

    7 день закончен. Работу выполнен

--- script/script.js
+++ script/script.js
@@ -228,3 +228,62 @@ hiddenContainer.addEventListener(`mouseout`, function () {
 hiddenContainer.addEventListener(`mouseover`, function () {
 	hiddenContainer.classList.add(`show-hidden-menu`);
 })
+
+//mobile-menumobile__list--drop-dropdown
+const burger = document.querySelector(`.burger__menu`);
+const burgerMobile = document.querySelector(`.burger__menu__box`);
+const body = document.querySelector(`body`);
+const mobileMenu = document.querySelector(`.mobile__menu`)
+const mobileList = document.querySelector(`.mobile__list`);
+const dropdown = document.querySelector(`.mobile__list--dropdown`);
+const mobileBack = document.querySelector(`.mobile-black__link`);
+
+
+const scrollTop = function () {
+	mobileMenu.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	})
+}
+
+const initialMenu = function (event) {
+	document.querySelector(`.mobile__list--dropdown`).classList.remove(`dropdown-tranformation`);
+	document.querySelector(`.mobile__nav`).querySelector(`.mobile__list`).classList.remove(`mobile__nav-tranformation`);
+	scrollTop();
+}
+
+burger.addEventListener(`click`, function () {
+	mobileMenu.classList.add(`open-mobile-menu`);
+	body.classList.add(`lock`);
+	initialMenu();
+})
+burgerMobile.addEventListener(`click`, function () {
+	mobileMenu.classList.remove(`open-mobile-menu`)
+	body.classList.remove(`lock`);
+})
+
+mobileList.addEventListener(`click`, function (event) {
+	console.log(event.target);
+	if (event.target.classList.contains(`mobile__link--drop`)) {
+		event.preventDefault();
+		event.target.closest(`.mobile__list`).classList.add(`mobile__nav-tranformation`);
+		event.target.closest(`.mobile__item`).querySelector(`.mobile__list--dropdown`).classList.add(`dropdown-tranformation`);
+		scrollTop();
+	}
+	if (event.target.classList.contains(`catalog__open`)) {
+		event.preventDefault();
+		event.target.closest(`.mobile__list--dropdown`).classList.remove(`dropdown-tranformation`);
+		event.target.closest(`.dropdown-submenu__list-item`).querySelector(`.mobile__list--drop-dropdown`).classList.add(`drop-dropdown-tranformation`);
+	}
+	if (event.target.classList.contains(`mobile-back__link`)) {
+		event.preventDefault();
+		event.target.closest(`.mobile__list--dropdown`).classList.remove(`dropdown-tranformation`);
+		event.target.closest(`.mobile__nav`).querySelector(`.mobile__list`).classList.remove(`mobile__nav-tranformation`);
+		scrollTop();
+	}
+	if (event.target.classList.contains(`drop-drop-back__link`)) {
+		event.preventDefault();
+		event.target.closest(`.mobile__list--dropdown`).classList.add(`dropdown-tranformation`);
+		event.target.closest(`.dropdown-submenu__list-item`).querySelector(`.mobile__list--drop-dropdown`).classList.remove(`drop-dropdown-tranformation`);
+	}
+})
