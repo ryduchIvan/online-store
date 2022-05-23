dae9030f909c3f6e3bf568a56ae43783af00e49f
Ivan Rudnichenki <rudnicenko.ivan@gmail.com>
2022-05-23 18:57:45 +0300

    закончил работу на сегодня ( 95 % задачу выполнил )

--- product/js/select.js
+++ product/js/select.js
@@ -1,9 +1,11 @@
 const mainSelect = document.querySelectorAll(`.main__select`);
 const mainSelectArrow = document.querySelector(`.button__select_arrow`);
-const selectBox = document.querySelector(`.select__box`);
+const selectBox = document.querySelectorAll(`.select__box`);
+const selectButton = document.querySelectorAll(`.button__select`);
 
-mainSelect.forEach(function (item) {
-	item.addEventListener(`click`, function (event) {
-		item.nextElementSibling.classList.toggle = 'show-select-box';
+mainSelect.forEach(function (elem) {
+	elem.addEventListener(`click`, function () {
+		elem.nextElementSibling.classList.toggle(`show-select-box`);
+		elem.childNodes[3].childNodes[1].classList.toggle(`rotate-arrow-select`);
 	})
 })
