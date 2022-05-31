45b7161f7c5c15faa4f7e8d84e1e3e5775a43eb0
Ivan Rudnichenki <rudnicenko.ivan@gmail.com>
2022-05-31 19:56:34 +0300

    Закрпил написание кода, изменили стили корзини

--- catalog/js/bag.js
+++ catalog/js/bag.js
@@ -1,10 +1,10 @@
 const buttonBag = document.querySelectorAll(`.in_the_bag`);
 const bagProductList = document.querySelector(`.bag__content_list`);
-const bagText = document.querySelector(`.bag__tex`);
 const bagQuantity = document.querySelector(`.bag_quanity`);
-const fullPrce = document.querySelector(`.fullprice`);
-
+const fullpirce = document.querySelector(`.fullprice`);
+const bagText = document.querySelector(`.bag__tex`);
 let priceBag = 0;
+
 const randomId = () => {
 	return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);//рандомное id товаров 
 };
@@ -17,102 +17,83 @@ const normalPrice = (str) => {
 	return String(str).replace(/(\d)(?=(\d\d\d)+([\d]|$))/g, '$1 ');// добавляет пробелы
 }
 
-const generateCartProduct = (img, title, price, id) => {
+const generateCartProduct = function (id, img, title, price) {
 	return `
-	<li class="bag__content_item">
-	<article class="bag__content_product" data-id = "${id}">
-		<img src="${img}" alt=""
-			class="bag__product_img">
-		<div class="bag__porduct_text">
-			<h3 class="bag__product_title">
-				${title}
-			</h3>
-			<div class="bag__product_price">${normalPrice(price)} ₴</div>
+<li class="bag__content_item">
+<article class="bag__content_product" data-id ="${id}">
+	<img src="${img}" alt=""
+		class="bag__product_img">
+	<div class="bag__porduct_text">
+		<h3 class="bag__product_title">
+		${title}
+		</h3>
+		<div class="bag__product_price">${normalPrice(price)} ₴</div>
+	</div>
+	<div class="bag__product_icon">
+		<div class="bag__product_starts">
+			<img src="../../main/img/icon/Star 5 (1).svg" alt="">
+			<img src="../../main/img/icon/Star 5 (1).svg" alt="">
+			<img src="../../main/img/icon/Star 5 (1).svg" alt="">
+			<img src="../../main/img/icon/Star 5 (1).svg" alt="">
+			<img src="../../main/img/icon/Star 5 (1).svg" alt="">
 		</div>
-		<div class="bag__product_icon">
-			<div class="bag__product_starts">
-				<img src="../../main/img/icon/Star 5 (1).svg" alt="">
-				<img src="../../main/img/icon/Star 5 (1).svg" alt="">
-				<img src="../../main/img/icon/Star 5 (1).svg" alt="">
-				<img src="../../main/img/icon/Star 5 (1).svg" alt="">
-				<img src="../../main/img/icon/Star 5 (1).svg" alt="">
-			</div>
-			<div  araia-label="Удалить товар">
-				<img src="../../product/img/icon/delete.svg" alt="" class="bag__product_delete">
-			</div>
+		<div  araia-label="Удалить товар">
+			<img src="../../product/img/icon/delete.svg" alt="" class="bag__product_delete">
 		</div>
-	</article>
-</li>
-
+	</div>
+</article>
+</li>	
 	`
 }
-
-const sumPriceBag = function (priceFun) {
-	priceBag += priceFun;
+const bagSum = function (price) {
+	priceBag += price;
 }
-const minusPriceBag = function (priceFun) {
-	priceBag -= priceFun;
+const bagMinus = function (price) {
+	priceBag -= price;
 }
 const printFullPrice = function () {
-	fullPrce.textContent = normalPrice(priceBag) + ' ₴';
+	fullpirce.textContent = `${normalPrice(priceBag)} ₴`;
 }
 const changeQuantity = function () {
 	let length = bagProductList.children.length;
-	console.log(length);
 	bagQuantity.textContent = length;
 	if (length > 0) {
 		bagText.classList.add(`active`);
 	} else {
 		bagText.classList.remove(`active`);
 	}
-	//if (length = 0) {
-	//	document.querySelector(`.bag__content_item`).classList.add(`new-flex-style`);
-	//} else {
-	//	document.querySelector(`.bag__content_item`).classList.remove(`new-flex-style`)
-	//}
 }
-changeQuantity();
+
 buttonBag.forEach(function (elem) {
 	elem.closest(`.catalog__item`).setAttribute(`data-id`, randomId());
 	elem.addEventListener(`click`, function (item) {
 		let self = item.currentTarget;
 		let parent = self.closest(`.catalog__item`);
+		let id = parent.dataset.id;
 		let img = parent.querySelector(`.catalog__img_img`).getAttribute(`src`);
 		let title = parent.querySelector(`.knife__title`).textContent;
 		let price = parseInt(priceWithoutSpaces(parent.querySelector(`.knife__price`).textContent));
-		let id = parent.dataset.id;
-		//Добавление товара в корзину
-		bagProductList.insertAdjacentHTML(`afterbegin`, generateCartProduct(img, title, price, id));
-		//сумма 
-		sumPriceBag(price);
-		//Вивели итоговую сумму в блок
+
+		bagProductList.insertAdjacentHTML(`afterbegin`, generateCartProduct(id, img, title, price));
+		bagSum(price);
 		printFullPrice();
-		//изменение количества продуктов в корзине
 		changeQuantity();
-		//делаем так что бы мы могли добавить конкретный товар только один раз 
-		self.disabled = true;
+		elem.disabled = true;
 	})
 })
 
-//удаление товара из корзини 
-
-const deleteItemBag = function (item) {
-	//раздизайблили кнопку "в корзину"
-	let id = document.querySelector(`.bag__content_product`).dataset.id;
-	document.querySelector(`.catalog__item[data-id = "${id}"]`).querySelector(`.in_the_bag`).disabled = false;
-	//минисуем цену
-	let price = parseInt(priceWithoutSpaces(item.querySelector(`.bag__product_price`).textContent))
-	minusPriceBag(price);
-	//Выводим итоговую суму
+const deleteItem = function (a) {
+	let idPoruduct = a.querySelector(`.bag__content_product`).dataset.id;
+	document.querySelector(`.catalog__item[data-id = "${idPoruduct}"]`).querySelector(`.in_the_bag`).disabled = false;
+	let price = parseInt(priceWithoutSpaces(a.querySelector(`.bag__product_price`).textContent));
+	bagMinus(price);
 	printFullPrice();
-	//убераем элемент
-	item.remove();
-	//меняем количество значение количества элементов 
+	a.remove();
 	changeQuantity();
 }
 
 bagProductList.addEventListener(`click`, function (event) {
 	if (event.target.classList.contains(`bag__product_delete`)) {
-		deleteItemBag(event.target.closest(`.bag__content_item`));
+		deleteItem(event.target.closest(`.bag__content_item`))
 	}
 })
